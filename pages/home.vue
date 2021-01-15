4f425af6b118475e55d42d29131d879d52152b08
Subhadeep Chel <43606003+chelsubhadeep@users.noreply.github.com>
2021-01-15 20:59:14 +0530

    breakpoints and visibility

--- pages/home.vue
+++ pages/home.vue
@@ -2,28 +2,9 @@
   <v-app>
     <div class="home">
       <h1>Home Page</h1>
-      <v-btn disabled class="pink white--text">click me</v-btn>
-      <v-btn depressed class="pink">click me</v-btn>
-      <v-btn flat class="pink">click me</v-btn>
-      <v-btn depressed class="pink white--text">
-        <v-icon left>mdi-email</v-icon>
-        <span>Email Me</span>
-      </v-btn>
-      <v-btn depressed small class="pink white--text">
-        <v-icon small left>mdi-email</v-icon>
-        <span>Email Me</span>
-      </v-btn>
-      <v-btn depressed large class="pink white--text">
-        <span>Email Me</span>
-        <v-icon right large>mdi-email</v-icon>
-      </v-btn>
-      <v-btn depressed small dark color="purple">
-        <span>Email Me</span>
-        <v-icon right large>mdi-email</v-icon>
-      </v-btn>
-      <v-btn fab depressed small dark color="purple">
-        <v-icon>mdi-cards-heart</v-icon>
-      </v-btn>
+      <!-- <v-btn class="hidden-md-and-down">Click Me</v-btn>
+      <v-btn class="hidden-md-and-up">Ok</v-btn> -->
+      <v-btn class="hidden-sm-only">Enter</v-btn>
     </div>
   </v-app>
 </template>
